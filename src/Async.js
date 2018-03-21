c4a2f8071ed5a2e233794b522eeadec21b515b70
Wellington Guimaraes <wellguimaraes@gmail.com>
2018-03-21 17:41:29 +0100

    improved promises replacement

--- src/Async.js
+++ src/Async.js
@@ -5,17 +5,6 @@ function toArray(it) {
   return Array.isArray(it) ? it : [ it ]
 }
 
-export const switchAsync = (promised, cases) => {
-  if (promised.pending)
-    return cases.pending && cases.pending()
-
-  if (promised.error)
-    return cases.failed && cases.failed()
-
-  return cases.success && cases.success()
-}
-
-
 export default class Async extends React.Component {
 
   constructor(props) {
@@ -23,18 +12,24 @@ export default class Async extends React.Component {
 
     const values = toArray(props.value || props.values)
 
-    this.state = {
-      results: values.map(() => ({
+    this.state = values
+      .map(() => ({
         pending: true,
         error  : undefined,
-        value  : undefined,
-        case(cases) { return switchAsync(this, cases) }
+        value  : undefined
       }))
-    }
+      .reduce((acc, curr, index) => {
+        acc[ index ] = curr
+        return acc
+      }, {})
+
+    this.handlePromise = this.handlePromise.bind(this)
   }
+
 
   componentDidMount() {
     const currValues = toArray(this.props.value || this.props.values)
+
     currValues.forEach((it, i) => this.handlePromise(it, i))
   }
 
@@ -43,64 +38,59 @@ export default class Async extends React.Component {
     const nextValues = toArray(nextProps.value || nextProps.values)
 
     nextValues.forEach((it, i) => {
-      if (currValues[ i ] !== it)
+      if (currValues[ i ] !== it) {
         this.handlePromise(it, i)
+      }
     })
   }
 
-  handlePromise = (promise, index) => {
+  handlePromise(promise, index) {
     this.setState({
-      results: this.state.results.map((it, i) =>
-        i === index
-          ? {
-            pending: true,
-            error  : undefined,
-            value  : undefined,
-            case(cases) {
-              return switchAsync(this, cases)
-            }
-          }
-          : it)
-    })
+      [ index ]: {
+        pending : true,
+        error   : undefined,
+        value   : undefined,
+        _promise: promise
+      }
+    }, () => {
+      Promise
+        .resolve(promise)
+        .then((value) => {
+          const currPromise = this.state[ index ]._promise
 
-    Promise
-      .resolve(promise)
-      .then((value) => {
-        this.setState({
-          results: this.state.results.map((it, i) => i === index
-            ? {
-              value,
-              pending: false,
-              error  : undefined,
-              case(cases) {
-                return switchAsync(this, cases)
+          if (!currPromise || currPromise === promise)
+            this.setState({
+              [ index ]: {
+                value,
+                pending: false,
+                error  : undefined
               }
-            }
-            : it)
-        })
-      }, (error) => {
-        this.setState({
-          results: this.state.results.map((it, i) => i === index
-            ? {
-              error,
-              pending: false,
-              value  : undefined,
-              case(cases) {
-                return switchAsync(this, cases)
+            })
+        }, (error) => {
+          const currPromise = this.state[ index ]._promise
+
+          if (!currPromise || currPromise === promise)
+            this.setState({
+              [ index ]: {
+                value  : undefined,
+                pending: false,
+                error
               }
-            }
-            : it)
+            })
         })
-      })
+    })
+  }
+
+  get results() {
+    return this.props.values.map((ignore, i) => this.state[ i ])
   }
 
   render() {
-    const { results } = this.state
-
-    return this.props.children.apply(null, results)
+    return this.props.children.apply(null, this.results)
   }
 }
 
 Async.propTypes = {
-  children: PropTypes.func.isRequired
+  children: PropTypes.func,
+  values  : PropTypes.array
 }
