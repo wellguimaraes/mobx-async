5e514424c3e45d5348ce0dbe9a919fa705f7251f
Wellington Guimaraes <wellguimaraes@gmail.com>
2018-04-10 13:49:02 +0200

    removed Async component and added asyncComputed decorator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-import { action, observable } from 'mobx'
-import _Async from './Async'
+import { action, computed, observable, reaction } from 'mobx'
 
-export const Async = _Async
+export function dependsOn(...anything) { }
 
-export function dependsOn(anything) { }
+export function asyncComputed(target, key, descriptor) {
+  const originalGetter = descriptor.get
+
+  const obsValue   = observable.box(undefined)
+  const obsPending = observable.box(false)
+  const obsError   = observable.box(undefined)
+
+  const pendingKey = key + 'Pending'
+  const errorKey = key + 'Error'
+
+  // noinspection JSUnresolvedFunction
+  Object.defineProperty( target, pendingKey, computed(target, pendingKey, {
+    get() {
+      return obsPending.get()
+    }
+  }))
+
+  // noinspection JSUnresolvedFunction
+  Object.defineProperty( target,errorKey,computed(target, errorKey, {
+    get() {
+      return obsError.get()
+    }
+  }))
+
+  async function computer() {
+    obsPending.set(true)
+
+    try {
+      let value = await originalGetter.call(this)
+      obsValue.set(value)
+      obsPending.set(false)
+      obsError.set(undefined)
+    } catch (err) {
+      obsPending.set(false)
+      obsError.set(err)
+    }
+  }
+
+  let firstTime = true
+
+  return computed(target, key, {
+    get() {
+      if (firstTime) {
+        firstTime = false
+        reaction(() => computer.call(this), () => {})
+      }
+
+      return obsValue.get()
+    }
+  })
+}
 
 export function asyncAction(target, key, descriptor) {
   const original = descriptor.value
